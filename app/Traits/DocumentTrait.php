ded09eb749d821558b67043e419e89b2f2d7137e
Julian Rodriguez <julianlink123@hotmail.com>
2025-06-12 23:43:55 -0500

    proceso de creacion PDF 1

--- app/Traits/DocumentTrait.php
+++ app/Traits/DocumentTrait.php
@@ -5,7 +5,6 @@ namespace App\Traits;
 use App\Http\Controllers\Api\ConfigurationController;
 use App\Models\Company;
 use App\Models\Resolution;
-use QrCode;
 use Exception;
 use DOMDocument;
 use InvalidArgumentException;
@@ -16,6 +15,13 @@ use ubl21dian\Sign;
 use ZipArchive;
 use App\Custom\zipfileDIAN;
 use App\Models\TypeDocument;
+use Illuminate\Support\Facades\View;
+use Mpdf\Config\ConfigVariables;
+use Mpdf\Config\FontVariables;
+use Mpdf\HTMLParserMode;
+use Mpdf\Mpdf;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
 
 trait DocumentTrait
 {
@@ -316,18 +322,18 @@ trait DocumentTrait
         else
             $totalbase = $request->legal_monetary_totals['line_extension_amount'];
 
-        //Si tipo de documento es invoice o POS
-        if($tipodoc == "INVOICE" || $tipodoc == "POS"){
+        //CALCULAR QR EN BASE-64 PARA HAB Y PRODUCCION
+        if($tipodoc == "INVOICE" || $tipodoc == "POS"){//Si tipo de documento es invoice o POS
             if($company->type_environment_id == 2){ //si es ambiente de pruebas
-                if(isset($request->tax_totals[0]['tax_amount'])){
-                    $qrBase64 = base64_encode(QrCode::format('png')
+                if(isset($request->tax_totals[0]['tax_amount'])){ //si esta definido tax total, se establece el impuesto IVA
+                    $qrBase64 = base64_encode(QrCode::format('png') //me codifica en base 64 el qr del png y establece URL de habilitacion
                                             ->errorCorrection('Q')
                                             ->size(220)
                                             ->margin(0)
                                             ->generate('NumFac: '.$request->number.PHP_EOL.'FecFac: '.$date.PHP_EOL.'NitFac: '.$company->identification_number.PHP_EOL.'DocAdq: '.$customer->company->identification_number.PHP_EOL.'ValFac: '.$request->legal_monetary_totals['tax_exclusive_amount'].PHP_EOL.'ValIva: '.$request->tax_totals[0]['tax_amount'].PHP_EOL.'ValOtroIm: 0.00'.PHP_EOL.'ValTotal: '.$request->legal_monetary_totals['payable_amount'].PHP_EOL.'CUFE: '.$cufecude.PHP_EOL.'https://catalogo-vpfe-hab.dian.gov.co/document/searchqr?documentkey='.$cufecude));
                     $QRStr = 'NumFac: '.$request->number.PHP_EOL.'FecFac: '.$date.PHP_EOL.'NitFac: '.$company->identification_number.PHP_EOL.'DocAdq: '.$customer->company->identification_number.PHP_EOL.'ValFac: '.$request->legal_monetary_totals['tax_exclusive_amount'].PHP_EOL.'ValIva: '.$request->tax_totals[0]['tax_amount'].PHP_EOL.'ValOtroIm: 0.00'.PHP_EOL.'ValTotal: '.$request->legal_monetary_totals['payable_amount'].PHP_EOL.'CUFE: '.$cufecude.PHP_EOL.'https://catalogo-vpfe-hab.dian.gov.co/document/searchqr?documentkey='.$cufecude;
                 }
-                else{
+                else{ //No se establece el impuesto IVA y establece URL de habilitacion
                     $qrBase64 = base64_encode(QrCode::format('png')
                                             ->errorCorrection('Q')
                                             ->size(220)
@@ -337,15 +343,15 @@ trait DocumentTrait
                 }
             }
             else{ // si es produccion
-                if(isset($request->tax_totals[0]['tax_amount'])){
-                    $qrBase64 = base64_encode(QrCode::format('png')
+                if(isset($request->tax_totals[0]['tax_amount'])){ //si esta definido tax total
+                    $qrBase64 = base64_encode(QrCode::format('png') //se establece el impuesto IVA y URL de produccion, QR codificado en base 64
                                             ->errorCorrection('Q')
                                             ->size(220)
                                             ->margin(0)
                                             ->generate('NumFac: '.$request->number.PHP_EOL.'FecFac: '.$date.PHP_EOL.'NitFac: '.$company->identification_number.PHP_EOL.'DocAdq: '.$customer->company->identification_number.PHP_EOL.'ValFac: '.$request->legal_monetary_totals['tax_exclusive_amount'].PHP_EOL.'ValIva: '.$request->tax_totals[0]['tax_amount'].PHP_EOL.'ValOtroIm: 0.00'.PHP_EOL.'ValTotal: '.$request->legal_monetary_totals['payable_amount'].PHP_EOL.'CUFE: '.$cufecude.PHP_EOL.'https://catalogo-vpfe.dian.gov.co/document/searchqr?documentkey='.$cufecude));
                     $QRStr = 'NumFac: '.$request->number.PHP_EOL.'FecFac: '.$date.PHP_EOL.'NitFac: '.$company->identification_number.PHP_EOL.'DocAdq: '.$customer->company->identification_number.PHP_EOL.'ValFac: '.$request->legal_monetary_totals['tax_exclusive_amount'].PHP_EOL.'ValIva: '.$request->tax_totals[0]['tax_amount'].PHP_EOL.'ValOtroIm: 0.00'.PHP_EOL.'ValTotal: '.$request->legal_monetary_totals['payable_amount'].PHP_EOL.'CUFE: '.$cufecude.PHP_EOL.'https://catalogo-vpfe.dian.gov.co/document/searchqr?documentkey='.$cufecude;
                 }
-                else{
+                else{ //No se establece el impuesto IVA y establece URL de produccion
                     $qrBase64 = base64_encode(QrCode::format('png')
                                             ->errorCorrection('Q')
                                             ->size(220)
@@ -354,6 +360,120 @@ trait DocumentTrait
                     $QRStr = 'NumFac: '.$request->number.PHP_EOL.'FecFac: '.$date.PHP_EOL.'NitFac: '.$company->identification_number.PHP_EOL.'DocAdq: '.$customer->company->identification_number.PHP_EOL.'ValFac: '.$request->legal_monetary_totals['tax_exclusive_amount'].PHP_EOL.'ValIva: 0.00'.PHP_EOL.'ValOtroIm: 0.00'.PHP_EOL.'ValTotal: '.$request->legal_monetary_totals['payable_amount'].PHP_EOL.'CUFE: '.$cufecude.PHP_EOL.'https://catalogo-vpfe.dian.gov.co/document/searchqr?documentkey='.$cufecude;
                 }
             }
-        }
+
+             $imageQr    =  "data:image/png;base64, ".$qrBase64;
+
+            if($template_json){
+                    if($tipodoc == 'POS')
+                        $pdf = $this->initMPdf('pos', $template_pdf);
+                    else
+                        $pdf = $this->initMPdf('invoice', $template_pdf);
+                    $pdf->SetHTMLHeader(View::make("pdfs.".strtolower($tipodoc).".header".$template_pdf, compact("user", "company", "customer", "resolution", "date", "time", "paymentForm", "request", "cufecude", "imageQr", "imgLogo", "withHoldingTaxTotal", "notes", "healthfields", "firma_facturacion")));
+                    $pdf->SetHTMLFooter(View::make("pdfs.".strtolower($tipodoc).".footer".$template_pdf, compact("user", "company", "customer", "resolution", "date", "time", "paymentForm", "request", "cufecude", "imageQr", "imgLogo", "withHoldingTaxTotal", "notes", "healthfields", "firma_facturacion")));
+                    $pdf->WriteHTML(View::make("pdfs.".strtolower($tipodoc).".template".$template_pdf, compact("user", "company", "customer", "resolution", "date", "time", "paymentForm", "request", "cufecude", "imageQr", "imgLogo", "withHoldingTaxTotal", "notes", "healthfields", "firma_facturacion")), HTMLParserMode::HTML_BODY);
+            }else{
+                if($tipodoc == 'POS')
+                    $pdf = $this->initMPdf('pos', $template_pdf);
+                else
+                    $pdf = $this->initMPdf();
+                    $pdf->SetHTMLHeader(View::make("pdfs.".strtolower($tipodoc).".header", compact("user", "company", "customer", "resolution", "date", "time", "paymentForm", "request", "cufecude", "imageQr", "imgLogo", "withHoldingTaxTotal", "notes", "healthfields", "firma_facturacion")));
+                    $pdf->SetHTMLFooter(View::make("pdfs.".strtolower($tipodoc).".footer", compact("user", "company", "customer", "resolution", "date", "time", "paymentForm", "request", "cufecude", "imageQr", "imgLogo", "withHoldingTaxTotal", "notes", "healthfields", "firma_facturacion")));
+                    $pdf->WriteHTML(View::make("pdfs.".strtolower($tipodoc).".template".$template_pdf, compact("user", "company", "customer", "resolution", "date", "time", "paymentForm", "request", "cufecude", "imageQr", "imgLogo", "withHoldingTaxTotal", "notes", "healthfields", "firma_facturacion")), HTMLParserMode::HTML_BODY);
+            }
+
+                $filename = storage_path("app/public/{$company->identification_number}/FES-{$resolution->next_consecutive}.pdf");
+             
+        }//fin tipo de documentos INVOICE o POS
+
+
     }
+
+
+    protected function initMPdf(string $type = 'invoice', string $template = null): Mpdf
+    //$type: tipo de documento (invoice, pos),     $template: numero/idenificador de plantilla es opcional
+    {
+        //Configuración Inicial de Fuentes, Obtiene configuraciones predeterminadas de mPDF
+        $defaultConfig = (new ConfigVariables())->getDefaults();
+        $fontDirs = $defaultConfig['fontDir'];
+        $defaultFontConfig = (new FontVariables())->getDefaults();
+        $fontData = $defaultFontConfig['fontdata'];
+
+        //Dimensiones de Página, Define tamaños diferentes para facturas vs tickets POS
+        if($type == 'pos'){
+            $pageWidth = 80;    // Ancho para POS (tickets)
+            $pageHeight = 297;  // Largo estándar para rollos
+        }
+        else{
+            $pageWidth = 219;   // Ancho carta en mm
+            $pageHeight = 279;  // Alto carta en mm
+        }
+
+        //Configuración de Márgenes
+        $margin_left = '5';
+        $margin_right = '5';
+        $margin_top = '60';
+        $margin_bottom = '40';
+
+        //Carga de Configuración Personalizada (JSON) si existe deacuerdo a variable $template
+        $filename = base_path('resources/views/pdfs/' . $type . '/config'.$template.'.json');
+        if (file_exists($filename)) {
+            $jsonD =  file_get_contents('config'.$template.'.json');
+            $margin = json_decode($jsonD,true);
+            if(isset($margin)){
+                $margin_top = $margin['top'];
+                $margin_right = $margin['right'];
+                $margin_bottom = $margin['bottom'];
+                $margin_left = $margin['left'];
+            }
+        }
+        if($template){
+            $pdf = new Mpdf([
+                'fontDir' => array_merge($fontDirs, [
+                    base_path('public/fonts/roboto/'),
+                ]),
+                'fontdata' => $fontData + [
+                    'Roboto' => [
+                        'R' => 'Roboto-Regular.ttf',
+                        'B' => 'Roboto-Bold.ttf',
+                        'I' => 'Roboto-Italic.ttf',
+                    ]
+                ],
+                'default_font' => 'Roboto',
+                'margin_left' => $margin_left,
+                'margin_right' => $margin_right,
+                'margin_top' => $margin_top,
+                'margin_bottom' => $margin_bottom ,
+                'margin_header' => 5,
+                'margin_footer' => 2,
+                'format' => [$pageWidth, $pageHeight], // Establece el tamaño de la página
+            ]);
+        }
+        else{
+            $pdf = new Mpdf([
+                'fontDir' => array_merge($fontDirs, [
+                    base_path('public/fonts/roboto/'),
+                ]),
+                'fontdata' => $fontData + [
+                    'Roboto' => [
+                        'R' => 'Roboto-Regular.ttf',
+                        'B' => 'Roboto-Bold.ttf',
+                        'I' => 'Roboto-Italic.ttf',
+                    ]
+                ],
+                'default_font' => 'Roboto',
+                'margin_left' => 5,
+                'margin_top' => 35,
+                'margin_bottom' => 5,
+                'margin_header' => 5,
+                'margin_footer' => 2,
+                'format' => [$pageWidth, $pageHeight], // Establece el tamaño de la página
+            ]);
+        }
+        if($template)
+            $pdf->WriteHTML(file_get_contents(base_path('resources/views/pdfs/' . $type . '/styles'.$template.'.css')), HTMLParserMode::HEADER_CSS);
+        else
+            $pdf->WriteHTML(file_get_contents(base_path('resources/views/pdfs/' . $type . '/styles.css')), HTMLParserMode::HEADER_CSS);
+        return $pdf;
+    }
+    
 }
